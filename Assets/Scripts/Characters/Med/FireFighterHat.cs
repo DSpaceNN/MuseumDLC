f5abad6c48879bf496944bf6d35e1d2f451d2602
AlekseyBrovko <96891019+AlekseyBrovko@users.noreply.github.com>
2024-11-21 16:46:48 +0300

    тяжеленный пожарник

---
+++ Assets/Scripts/Characters/Med/FireFighterHat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FireFighterHat : ItemOnCharacterMb
+{
+    [SerializeField] private GameObject _hairsGo;
+
+    public override void OnEquip() 
+    {
+        SkinnedMeshRenderer renderer = _hairsGo.GetComponent<SkinnedMeshRenderer>();
+        renderer.SetBlendShapeWeight(0, 100f);
+    }
+}
